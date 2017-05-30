6b5732e8716c6f22bdb78708c03967ae3ea6e730
Meesun Park <meesun.park@rutgers.edu>
2017-05-29 22:21:31 -0400

    SetInterval is now working.
    Fixed to fix the glitch to only work once.

--- background.js
+++ background.js
@@ -1,66 +1,11 @@
-/*chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
-    switch(message.type) {
-        case "colors-div":
-            var divs = document.querySelectorAll("div");
-            if(divs.length === 0) {
-                alert("There are no any divs in the page.");
-            } else {
-                for(var i=0; i&lt;divs.length; i++) {
-                    divs[i].style.backgroundColor = message.color;
-                }
-            }
-        break;
-    }
-});*/
-
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-/*
-  Displays a notification with the current time. Requires "notifications"
-  permission in the manifest file (or calling
-  "Notification.requestPermission" beforehand).
-*/
 function show() {
-  var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
-  var hour = time[1] % 12 || 12;               // The prettyprinted hour.
-  var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
+  var roundReminder = setInterval(roundLogNotification(), 900000);
 
-  setInterval(roundLogReminder, 900000) 
-  
 }
 
-function roundLogReminder(){
-    new Notification(hour + time[2] + ' ' + period 'Time to do a Round Log!', {
-        icon: '48x48.png',
-        body: 'Don\'t forget to submit a round log every 15 minutes. If not, please enter a reason why in your next round!'
-    });
+function roundLogNotification(){
+    new Notification('Hello', {
+        icon: '48.png',
+        body: 'Time to make the toast.'
+  });
 }
-
-// Conditionally initialize the options.
-if (!localStorage.isInitialized) {
-  localStorage.isActivated = true;   // The display activation.
-  localStorage.frequency = 1;        // The display frequency, in minutes.
-  localStorage.isInitialized = true; // The option initialization.
-}
-// Test for notification support.
-if (window.Notification) {
-  // While activated, show notifications at the display frequency.
-  if (JSON.parse(localStorage.isActivated)) { show(); }
-  var interval = 0; // The display interval, in minutes.
-  setInterval(function() {
-    interval++;
-    if (
-      JSON.parse(localStorage.isActivated) &&
-        localStorage.frequency <= interval
-    ) {
-      show();
-      interval = 0;
-    }
-  }, 60000);
-}
-
-
-
-
-
