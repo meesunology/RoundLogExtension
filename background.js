06b1bc9aecbf628b617d94d2051527aac5280d57
Meesun Park <meesun.park@rutgers.edu>
2017-05-29 23:07:11 -0400

    Fixed the glitch for Notification Push.

--- background.js
+++ background.js
@@ -1,11 +1,32 @@
 function show() {
-  var roundReminder = setInterval(roundLogNotification(), 900000);
+  var roundLogReminder = setInterval(function() {
+    new Notification('Hey! It\'s time to do a round log!', {
+        icon: '48.png',
+        body: 'Press the extension to open up our round log.'
+    })
+    }, 900000);
 
 }
 
-function roundLogNotification(){
-    new Notification('Hello', {
-        icon: '48.png',
-        body: 'Time to make the toast.'
-  });
+// Conditionally initialize the options.
+if (!localStorage.isInitialized) {
+  localStorage.isActivated = true;   // The display activation.
+  localStorage.frequency = 1;        // The display frequency, in minutes.
+  localStorage.isInitialized = true; // The option initialization.
+}
+// Test for notification support.
+if (window.Notification) {
+  // While activated, show notifications at the display frequency.
+  if (JSON.parse(localStorage.isActivated)) { show(); }
+  var interval = 0; // The display interval, in minutes.
+  setInterval(function() {
+    interval++;
+    if (
+      JSON.parse(localStorage.isActivated) &&
+        localStorage.frequency <= interval
+    ) {
+      show();
+      interval = 0;
+    }
+  }, 60000);
 }
