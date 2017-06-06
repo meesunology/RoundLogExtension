c3a401da87e8ccc92bf823d85de9b2531aa1ec58
Meesun Park <meesun.park@rutgers.edu>
2017-06-06 13:10:01 -0400

    update background.js

--- background.js
+++ background.js
@@ -1,11 +1,12 @@
 function show() {
-  var roundLogReminder = setInterval(function() {
+  setInterval(roundLogReminder(), 900000); //15 minutes in miliseconds
+}
+
+function roundLogReminder() {
     new Notification('Hey! It\'s time to do a round log!', {
         icon: '48.png',
         body: 'Press the extension to open up our round log.'
-    })
-    }, 60000); //15 minutes in miliseconds
-
+    });
 }
 
 show();
