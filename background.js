c831f86979c748c9ff2a43e6f44890863cc89c44
Meesun Park <meesun.park@rutgers.edu>
2017-06-06 13:06:39 -0400

    Update Background.js

--- background.js
+++ background.js
@@ -8,25 +8,4 @@ function show() {
 
 }
 
-// Conditionally initialize the options.
-if (!localStorage.isInitialized) {
-  localStorage.isActivated = true;   // The display activation.
-  localStorage.frequency = 1;        // The display frequency, in minutes.
-  localStorage.isInitialized = true; // The option initialization.
-}
-// Test for notification support.
-if (window.Notification) {
-  // While activated, show notifications at the display frequency.
-  if (JSON.parse(localStorage.isActivated)) { show(); }
-  var interval = 0; // The display interval, in minutes.
-  setInterval(function() {
-    interval++;
-    if (
-      JSON.parse(localStorage.isActivated) &&
-        localStorage.frequency <= interval
-    ) {
-      show();
-      interval = 0;
-    }
-  }, 900000);
-}
+show();
